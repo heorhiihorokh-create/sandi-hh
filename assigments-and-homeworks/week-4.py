2e14b26bd70db76fd5740d96778276cfc1b929aa
Heorhii-Horokh <Heorhii.HOROKH@student.howest.be>
2026-03-05 15:59:52 +0100

    fix: week-4-assigment

--- assigments-and-homeworks/week-4.py
+++ assigments-and-homeworks/week-4.py
@@ -1,5 +1,3 @@
-# assigment week 4
-
 import smbus
 from RPi import GPIO
 import time
@@ -7,31 +5,30 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 i2c = smbus.SMBus(1)
-
-ADC_adress = 0x48
+ADC_address = 0x48
 
 commands_per_channel = {
-    #..
     2: 0b1001,
     3: 0b1101,
-    #..
 }
 
 channel = 2
 
 try:
     while True:
-        i2c.write_bute(ADC_adress,(commands_per_channel[channel]<<4) | 0x4)
+        i2c.write_byte(ADC_address, (commands_per_channel[channel] << 4) | 0x04)
 
-        analog_value = i2c.read_byte(ADC_adress)
+        # возможно нужен dummy read (зависит от твоего ADC)
+        # i2c.read_byte(ADC_address)
 
-        print("Channel 2 has  value of : {} and in volt this is {}".format(analog_value, analog_value / 255 *3.3))
+        analog_value = i2c.read_byte(ADC_address)
+        voltage = analog_value * 3.3 / 255
+
+        print(f"Channel {channel} has value: {analog_value} and in volt this is {voltage:.3f}")
         time.sleep(1)
 
-
-except KeyboardInterrupt as e:
+except KeyboardInterrupt:
     pass
 finally:
     i2c.close()
-    del i2c
     GPIO.cleanup()
